8bc13cab0fa9687f6c9743ae9aa664efe6640d26
Sourav Das <Souravds@users.noreply.github.com>
2021-07-16 17:25:39 +0600

    Create getNotes() and protect this with "protect" middeware
    Create getNotes() to get all notes initially
    Create protect middleware in authMiddleware to decode the token and authorized the user
    
    >>> login the user and get token from it and paste in postman to confirm the authorization

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const notes = require("./data/notes");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
@@ -13,22 +14,23 @@ connectDB();
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running");
+// });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
+// app.get("/api/notes", (req, res) => {
+//   res.json(notes);
+// });
 
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  res.send(note);
-});
+// app.get("/api/notes/:id", (req, res) => {
+//   const note = notes.find((n) => n._id === req.params.id);
+//   res.send(note);
+// });
 
 //^^^^^^THESE R THE API's^^^^^^^^^
 
