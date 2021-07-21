cce6c52886492fc869594d32ec0ad29a47011c0f
Sourav Das <Souravds@users.noreply.github.com>
2021-07-21 17:57:35 +0600

    make it better

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -15,6 +16,22 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
+
+// ----------------- DEPLOYMENT -------------------
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
+// ----------------- DEPLOYMENT -------------------
 
 app.use(notFound);
 app.use(errorHandler);
